694733a4091fc20efe74a444c83ee15c4e57395a
christian-crisologo-lrn <christian.crisologo@learnosity.com>
2020-08-24 03:28:14 +1000

    fix minor styles and effects

--- www/quiz.php
+++ www/quiz.php
@@ -21,7 +21,7 @@
     <div class="cover-container w-100 h-100 mx-auto flex-column"  > 
 
       <main role="main" class="inner cover" >
-        <img src="images/cover.png" class="img-fluid" />
+        <img src="images/cover.png" class="img-fluid cover-img"  style="display:none"   />
         <h1 class="cover-heading ff">Ready to defuse?</h1>
         <div class="player-selections">
           <div class="p-4 tech-b1"> 
@@ -49,26 +49,35 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
     <script>
-       const audioControl =  document.getElementById("bg-sound"); 
-      audioControl.volume = 0.5;
-      $('.enable-sound').on('click',(e)=>{
-          e.preventDefault();
-          console.log('#CC enable sounds ', audioControl);
-          if( !audioControl.paused){
-              audioControl.pause();
-              $('.enable-sound').html('Play 🔈'); 
+    
+      $(
+        ()=>{
+         
+          const audioControl =  document.getElementById("bg-sound"); 
+          audioControl.volume = 0.5;
+          $('.cover-img:hidden').fadeIn(3000);
 
-          }else{
-              console.log('#CC play ');
-              $('.enable-sound').html('Stop 🔇');
-              audioControl.play()
-          }
-      });
+          $('.enable-sound').on('click',(e)=>{
+              e.preventDefault();
+              if( !audioControl.paused){
+                  audioControl.pause();
+                  $('.enable-sound').html('Play 🔈'); 
+
+              }else{
+                  console.log('#CC play ');
+                  $('.enable-sound').html('Stop 🔇');
+                  audioControl.play()
+              }
+          });
+        }
+      );
+      window.scrollTo(0,0);
+     
     </script>
   </body>
 </html>
